79ba59c3f374201d20237ec19b220ada5e19c48a
msaad5746 <msaad5746@gmail.com>
2022-05-10 16:52:03 +0500

    initial commit

---
+++ border_pay/android/app/src/main/kotlin/com/example/border_pay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.border_pay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ school_pay/android/app/src/main/kotlin/com/example/school_pay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.school_pay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
